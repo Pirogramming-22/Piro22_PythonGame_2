6983cf27cd9b5ecc031d5c1d59c531232823d1c3
l-wanderer01 <jhjjang0423@naver.com>
2025-01-05 23:34:47 +0900

    [feat] getUserLife(), getFriends() 함수 로직 작성(3번, 4번)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 ##### Functions #####
+import random
+
 
 def printIntro():
     # - input: X
@@ -19,17 +21,65 @@ def getUserLife():
     # - input: X
     # -기능: (3)pdf 명세대로 출력
     # - return: 입력받은 주량 (단위: 잔, int)
-    print("유저의 주량은: 2")
-    return 2
-    pass
+    while(True):
+        try:
+            print(f"""
+        {'~'*20} 🍺 소주 기준 당신의 주량은?? 🍺 {'~'*20}
+        {' '*20} 🍺 1. 소주 반병 ( 2잔 )
+        {' '*20} 🍺 2. 소주 반병에서 한병 ( 4잔 )
+        {' '*20} 🍺 3. 소주 한병에서 한병 반 ( 6잔 )
+        {' '*20} 🍺 4. 소주 한병 반에서 두병 ( 8잔 )
+        {' '*20} 🍺 5. 소주 두병 이상 ( 10잔 )
+        """)
+            alcohol = int(input("당신의 치사량( 주량 )은 얼마만큼인가요?(1 ~ 5 중 입력해주세요) : "))
+    # 1 ~ 5 사이의 값이 아닌 것에 대한 예외처리
+        except ValueError:
+            print("😵‍💫 벌써부터 취하셨나요~~?? 😵‍💫 1 ~ 5 사이의 정수를 입력해주세요")
+        else:
+            if not (1 <= alcohol<= 5):
+                ("설마 소주 반병도 못먹는 술찌는 아니겠죠???🧐🧐 1 ~ 5 사이의 숫자를 입력해주세요!")
+            else:
+                print(f"유저의 주량은: {alcohol}\n")
+                break
+    return alcohol
 
 def getFriends():
     # - input: X
     # - 기능: (4)친구수 입력받기, pdf 명세대로 주량설정하고 출력, [스코어보드 출력, 게임리스트 출력은 메인에서 구현]
     # - return: 선택된 친구 정보 (dictionary: {name : 남은주량})
-    print("오늘 취할 친구들은: 은서 예진 연서")
-    return {"은서" : 2, "예진" : 8, "연서" : 6}
-    pass
+    while(True):
+        try:
+            friendsNum = int(input("함께 취할 친구들은 얼마나 필요하신가요?(사회적 거리두기로 인해 최대 3명까지 초대할 수 있어요!) : "))
+            print()
+        except ValueError:
+            print('⚠️'*20)
+            print("술 게임에 너무 설레어서 급하게 문자를 입력하신건가요??🤔 1 ~ 3까지의 숫자를 입력해주세요 😎")
+            print('⚠️'*20)
+            print('\n\n')
+        else:
+            if not (1 <= friendsNum <= 3):
+                print('⚠️'*20)
+                print("지금은 사회적 거리 두기 기간이라 4인 이상 모임은 금지예요. 😷")
+                print('⚠️'*20)
+                print('\n\n')
+            else:
+                break
+    
+    # 랜덤으로 게임에 참가할 친구 이름
+    friendsName = ["은서", "예진", "연서", "하연", "재훈", "수연", "은성", "태희", "명경"]
+    friendsName = [name for name in friendsName if name != userName] # 유저 이름 제외
+    
+    randomFriends = random.sample(friendsName, friendsNum)
+
+    # 랜덤 주량
+    alcohol = random.sample(range(1, 11), friendsNum)
+    
+    for i in range(friendsNum):
+        print(f"오늘 취할 친구는 {randomFriends[i]}입니다! (치사량 : {alcohol[i]})")
+    
+    selectedFriends = {randomFriends[i]: alcohol[i] for i in range(friendsNum)}
+
+    return selectedFriends
 
 def printScoreboard(players: dict):
     # - input: {유저이름 : 남은주량} 딕셔너리
