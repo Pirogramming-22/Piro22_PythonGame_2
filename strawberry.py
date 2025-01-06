1fa38b8b91fa3651418b8592ab0705940d3ddc57
qkrxogmla <suk05553@naver.com>
2025-01-06 16:33:49 +0900

    strawberry 인트로 추가

--- strawberry.py
+++ strawberry.py
@@ -7,6 +7,26 @@ def parktaehee_strawberry(userName: str, friends: tuple):
     current_turn = 0
     strawberry_count = 1
     increasing = True
+    print("""
+        ======================================
+            _ /—-\              /—-\_
+            \ \   \ —  - — —-  /   / /
+             \ \   \          /   / /
+            /  - - - -\ - - - —/ - ———  \\
+          /              ———              \\
+        |     ㅁ   ㅁ     ㅁ   ㅁ    ㅁ ㅁ     |
+        |     ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ     |
+        |       ㅁㅁㅁㅁㅁㅁㅁㅁ    ㅁ  ㅁ       |
+        \    ㅁ    ㅁ   ㅁ    ㅁㅁㅁㅁ   ㅁ    /
+          \      ㅁ  ㅁ    ㅁ ㅁ    ㅁㅁ    /
+            \        ㅁㅁㅁㅁㅁㅁㅁ        /
+              \        ㅁㅁㅁㅁㅁ       /
+                 \        ㅁ ㅁ     /
+                    \      ㅁ    /
+                       \       /
+                          ——-
+        ========================================
+        """)
 
     while True:
         player = players[current_turn % player_count]
@@ -21,13 +41,13 @@ def parktaehee_strawberry(userName: str, friends: tuple):
 
             if increasing:
                 if user_input != strawberry_count:
-                    print(f"누가 술을 마셔 {player}가 술을 마셔~")
+                    print(f"{player}이(가) 틀렸습니다! 게임을 종료합니다.")
                     return player
                 else:
                     strawberry_count += 1
             else:
                 if user_input != strawberry_count:
-                    print(f"누가 술을 마셔 {player}가 술을 마셔~")
+                    print(f"{player}이(가) 틀렸습니다! 게임을 종료합니다.")
                     return player
                 else:
                     strawberry_count -= 1
@@ -35,7 +55,7 @@ def parktaehee_strawberry(userName: str, friends: tuple):
             if random.random() < 0.2:
                 wrong_count = strawberry_count + random.choice([-2, -1, 1, 2])
                 print(f"{player}: {'딸기' * max(1, wrong_count)}")
-                print(f"{player}이(가) 틀렸습니다! 누가 술을 마셔 {player}가 술을 마셔~ 게임을 종료합니다.")
+                print(f"{player}이(가) 틀렸습니다! 게임을 종료합니다.")
                 return player
             print(f"{player}: {'딸기' * strawberry_count}")
 
